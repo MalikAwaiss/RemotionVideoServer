5de975a14ab233470b23131155022b64bf366ecf
Awais Malik <cibak@CIBAKs-MacBook-Pro-2.local>
2023-03-01 21:33:15 +0500

    video configured and api addedd

--- server.tsx
+++ server.tsx
@@ -8,7 +8,6 @@
 import fs from "fs";
 import os from "os";
 import path from "path";
-
 import { bundle } from "@remotion/bundler";
 import {
   getCompositions,
@@ -16,18 +15,135 @@ import {
   stitchFramesToVideo,
 } from "@remotion/renderer";
 import express from "express";
-
+var bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
+var cors = require('cors')
 const app = express();
 const port = process.env.PORT || 8000;
-const VIDEO_CATEGORIES = {
-  "Construction":"Construction",
-  "HairDresser":"HairDresser",
-  "Barber":"Barber",
+const payloadTemplate = {
+  "payload":{
+    videoCategory: 'string',
+    applicationId: 'string || number',
+    companyId: 'string || number',
+    videoData: 'object<VideoPayload>'
+  },
 }
-const compositionId = VIDEO_CATEGORIES.Construction;
+const VIDEO_CATEGORIES = {
+  "Construction":{
+    key:"Construction",
+    payload:{
+      customerName:'string',
+      companyName:'string',
+      loanAmount:'string',
+      installmentAmount:'string',
+      installmentFrequency:'string',
+      startDate:'string',
+      endDate:'string',
+    }
+  },
+  "Barber":{
+    key:"Barber",
+    payload:{
+      customerName:'string',
+      companyName:'string',
+      loanAmount:'string',
+      installmentAmount:'string',
+      installmentFrequency:'string',
+      startDate:'string',
+      endDate:'string',
+    }
+  },
+  "HairDresser":{
+    key:"HairDresser",
+    payload:{
+      customerName:'string',
+      companyName:'string',
+      loanAmount:'string',
+      installmentAmount:'string',
+      installmentFrequency:'string',
+      startDate:'string',
+      endDate:'string',
+    }
+  },
+}
+const compositionId = VIDEO_CATEGORIES.Construction.key;
+const outputDir = '/Users/cibak/Documents/AtomBits/RemotionVideoServer/output_video';
 
 const cache = new Map<string, string>();
 
+app.use(cors());
+app.get("/videoEnum",async (req,res)=>{
+  req.query;
+  res.writeHead(200)
+  res.write(JSON.stringify({...VIDEO_CATEGORIES,...payloadTemplate}))
+  res.end()
+})
+app.post('/generateVideo',jsonParser,async (req,res)=>{
+  try {
+    req.body
+    if(!req.body.applicationId || !req.body.videoCategory || !req.body.videoData){
+      res.writeHead(400,'Bad Request')
+      res.write(JSON.stringify({
+        message: 'Bad Request! payload invalid',
+        status: 400
+      }))
+      res.end()
+      return
+    }
+    const bundled = await bundle(path.join(__dirname, "./src/index.tsx"));
+    const comps = await getCompositions(bundled, { inputProps: req.query });
+    const videoIds = Object.keys(VIDEO_CATEGORIES);
+    const video = comps.find((c) => videoIds.includes(c.id.toString()));
+    const applicationId = req.body.applicationId;
+    if (!video) {
+      throw new Error(`No video called ${compositionId}`);
+    }
+    res.set("content-type", "video/mp4");
+
+    const tmpDir = await fs.promises.mkdtemp(
+      path.join(os.tmpdir(), "remotion-")
+    );
+    console.log('req.body.videoData',req.body.videoData)
+    const { assetsInfo } = await renderFrames({
+      config: video,
+      webpackBundle: bundled,
+      onStart: () => console.log("Rendering frames..."),
+      onFrameUpdate: (f) => {
+        if (f % 10 === 0) {
+          console.log(`Rendered frame ${f}`);
+        }
+      },
+      parallelism: null,
+      outputDir: tmpDir,
+      inputProps: req.body.videoData,
+      // compositionId,
+      imageFormat: "jpeg",
+    });
+    const fileName = applicationId+".mp4";
+    const finalOutput = path.join(outputDir, fileName);
+    await stitchFramesToVideo({
+      dir: tmpDir,
+      force: true,
+      fps: video.fps,
+      height: video.height,
+      width: video.width,
+      outputLocation: finalOutput,
+      internalOptions:{
+        imageFormat: "jpeg",
+        preferLossless:false,
+        preEncodedFileLocation:null
+      },
+      assetsInfo,
+    });
+    res.download(outputDir+'/'+fileName)
+    console.log("Video rendered and sent!");
+  } catch (err) {
+    console.error(err);
+    res.json({
+      error: err,
+    });
+  }
+})
 app.get("/", async (req, res) => {
   const sendFile = (file: string) => {
     // res.writeHead(200, { "Content-Length": 2000, "Content-Type": "video/mp4" });
@@ -53,6 +169,7 @@ app.get("/", async (req, res) => {
     const tmpDir = await fs.promises.mkdtemp(
       path.join(os.tmpdir(), "remotion-")
     );
+    const outputDir = '/Users/cibak/Documents/AtomBits/RemotionVideoServer/output_video'
     const { assetsInfo } = await renderFrames({
       config: video,
       webpackBundle: bundled,
@@ -69,7 +186,8 @@ app.get("/", async (req, res) => {
       imageFormat: "jpeg",
     });
 
-    const finalOutput = path.join(tmpDir, "out.mp4");
+    const finalOutput = path.join(outputDir, "out.mp4");
+    // const finalOutput = path.join(tmpDir, "out.mp4");
     await stitchFramesToVideo({
       dir: tmpDir,
       force: true,
@@ -84,8 +202,9 @@ app.get("/", async (req, res) => {
       },
       assetsInfo,
     });
-    cache.set(JSON.stringify(req.query), finalOutput);
-    sendFile(finalOutput);
+    // cache.set(JSON.stringify(req.query), finalOutput);
+    res.download(outputDir+'/out.mp4')
+    // sendFile(finalOutput);
     console.log("Video rendered and sent!");
   } catch (err) {
     console.error(err);
--- src/components/Construction/index.jsx
+++ src/components/Construction/index.jsx
@@ -55,7 +55,8 @@ const DURATIONS = {
     DURATION: 100,
   },
 };
-export const Construction = ({ titleText = "",midText="",secondMid="",ending="", titleColor = "" }) => {
+export const Construction = (props) => {
+  const videoData = {...props};
   // A <AbsoluteFill> is just a absolutely positioned <div>!
   return (
     <AbsoluteFill style={{ backgroundColor: "#FD6244" }}>
@@ -70,14 +71,14 @@ export const Construction = ({ titleText = "",midText="",secondMid="",ending="",
         from={DURATIONS.CUSTOMER_NAME.FROM}
         durationInFrames={DURATIONS.CUSTOMER_NAME.DURATION}
       >
-        <VideoWrapper videoFile={nameSlide} CustomComponent={<CustomerNameAnimation name={'Robert Pattison'} />} duration={DURATIONS.CUSTOMER_NAME} />
+        <VideoWrapper videoFile={nameSlide} CustomComponent={<CustomerNameAnimation name={videoData.customerName??'Robert Pattison'} />} duration={DURATIONS.CUSTOMER_NAME} />
         {/* <Intro duration={DURATIONS.INTRO} /> */}
       </Sequence>
       <Sequence
         from={DURATIONS.LOAN_AMOUNT.FROM}
         durationInFrames={DURATIONS.LOAN_AMOUNT.DURATION}
       >
-        <VideoWrapper videoFile={loanAmount} CustomComponent={<LoanAmountText amount={'5000£'} />} duration={DURATIONS.LOAN_AMOUNT} />
+        <VideoWrapper videoFile={loanAmount} CustomComponent={<LoanAmountText amount={videoData.loanAmount??'5000£'} />} duration={DURATIONS.LOAN_AMOUNT} />
         {/* <Intro duration={DURATIONS.INTRO} /> */}
       </Sequence>
       <Sequence
@@ -91,14 +92,14 @@ export const Construction = ({ titleText = "",midText="",secondMid="",ending="",
         from={DURATIONS.LOAN_FEATURES.FROM}
         durationInFrames={DURATIONS.LOAN_FEATURES.DURATION}
       >
-        <VideoWrapper videoFile={loanFeatures} CustomComponent={<LoanFeaturesText  companyName="Constructions Solutions Ltd." />} duration={DURATIONS.LOAN_FEATURES} />
+        <VideoWrapper videoFile={loanFeatures} CustomComponent={<LoanFeaturesText  companyName={videoData.companyName??"Constructions Solutions Ltd."} />} duration={DURATIONS.LOAN_FEATURES} />
         {/* <Intro duration={DURATIONS.INTRO} /> */}
       </Sequence>
       <Sequence
         from={DURATIONS.LOAN_INSTALLMENTS.FROM}
         durationInFrames={DURATIONS.LOAN_INSTALLMENTS.DURATION}
       >
-        <VideoWrapper videoFile={loanInstallments} CustomComponent={<LoanInstallmentsText  installmentText="188£ per working weekday" totalAmount="5000£" startDate="10/02/2023" endDate="20/04/2026" />} duration={DURATIONS.LOAN_INSTALLMENTS} />
+        <VideoWrapper videoFile={loanInstallments} CustomComponent={<LoanInstallmentsText  installmentText={videoData.installmentAmount?videoData.installmentAmount+' per working weekday':"188£ per working weekday"} totalAmount={videoData.loanAmount??'5000£'} startDate={videoData.startDate??"10/02/2023"} endDate={videoData.endDate?? "20/04/2026"} />} duration={DURATIONS.LOAN_INSTALLMENTS} />
         {/* <Intro duration={DURATIONS.INTRO} /> */}
       </Sequence>
       <Sequence
